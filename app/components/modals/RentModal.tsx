ae14d6635e968b6155aca79674e310f670f65ad2
Iffat Andriano <iffatandriano@IA-3.local>
2023-06-28 13:46:29 +0700

    add: component location and info steps

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,13 +1,17 @@
 'use client';
 
 import React, { useMemo, useState } from 'react';
-import Modal from './Modal';
+import { FieldValues, useForm } from 'react-hook-form';
 
 import useRentModal from '@/app/hooks/useRentModal';
+
 import Heading from '../Heading';
 import { categories } from '../navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
-import { FieldValues, useForm } from 'react-hook-form';
+import Modal from './Modal';
+import CountrySelect from '../inputs/CountrySelect';
+import dynamic from 'next/dynamic';
+import Counter from '../inputs/Counter';
 
 enum STEPS {
     CATEGORY = 0,
@@ -47,6 +51,14 @@ const RentModal = () => {
     })
 
     const category = watch('category');
+    const location = watch('location');
+    const guestCount = watch('guestCount');
+    const roomCount = watch('roomCount');
+    const bathroomCount = watch('bathroomCount');
+
+    const Map = useMemo(() => dynamic(() => import('../Map'), {
+        ssr: false
+    }), [location]);
 
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
@@ -105,11 +117,41 @@ const RentModal = () => {
         </div>
     )
 
+    if(step === STEPS.LOCATION) {
+        bodyContent = (
+            <div className='flex flex-col gap-8'>
+                <Heading
+                    title="Where is your place located?"
+                    subTitle='Help guests find you!'
+                />
+                <CountrySelect
+                    value={location}
+                    onChange={(value) => setCustomValue('location', value)}
+                />
+                <Map center={location?.latlng} />
+            </div>
+        )
+    }
+
+    if(step == STEPS.INFO) {
+        bodyContent = (
+            <div className='flex flex-col gap-8'>
+                <Heading
+                    title="Share some basics about your place"
+                    subTitle='What amenities do you have?'
+                />
+                <Counter title="Guests" subTitle='How many guests do you allow?' value={guestCount} onChange={(value) => setCustomValue('guestCount', value)} />
+                <Counter title="Rooms" subTitle='How many rooms do you have?' value={roomCount} onChange={(value) => setCustomValue('roomCount', value)} />
+                <Counter title="Bathrooms" subTitle='How many bathrooms do you have?' value={bathroomCount} onChange={(value) => setCustomValue('bathroomCount', value)} />
+            </div>
+        )
+    }
+
     return (
         <Modal 
             isOpen={rentModal.isOpen}
             onClose={rentModal.onClose}
-            onSubmit={rentModal.onClose}
+            onSubmit={onNext}
             actionLabel={actionLabel}
             secondaryActionLabel={secondaryActionLabel}
             secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
