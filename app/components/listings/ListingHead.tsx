e5ed1bb86447c4ac223c6ded7f90ef57f0b8a799
Iffat Andriano <iffatandriano@IA-3.local>
2023-07-06 01:53:14 +0700

    add: component listing info details

---
+++ app/components/listings/ListingHead.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import React from 'react';
+import Image from 'next/image';
+
+import { SafeUser } from '@/app/types';
+import useCountries from '@/app/hooks/useCountry';
+import Heading from '../Heading';
+import HeartButton from '../HeartButton';
+
+interface ListingHeadProps {
+    title: string,
+    imageSrc: string,
+    locationValue: string,
+    id: string;
+    currentUser?: SafeUser | null,
+}
+
+const ListingHead: React.FC<ListingHeadProps> = ({
+    title,
+    imageSrc,
+    locationValue,
+    id,
+    currentUser
+}) => {
+    const { getByValue } = useCountries();
+    const location = getByValue(locationValue);
+
+    return (
+        <>
+            <Heading
+                title={title}
+                subTitle={`${location?.region}, ${location?.label}`}
+            />
+            <div className='w-full h-[60vh] overflow-hidden rounded-xl relative'>
+                <Image
+                    alt="Image"
+                    src={imageSrc}
+                    fill
+                    className='object-cover w-full'
+                />
+                <div className='absolute top-5 right-5'>
+                    <HeartButton 
+                        listingId={id}
+                        currentUser={currentUser}
+                    />
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ListingHead;
