0b23ec938d80b1dae1be17074712a24f0b024730
Iffat Andriano <iffatandriano@IA-3.local>
2023-07-06 03:44:00 +0700

    add: comp reservations

---
+++ app/reservations/page.tsx
@@ -0,0 +1,50 @@
+import EmptyState from "../components/EmptyState";
+import ClientOnly from "../components/ClientOnly";
+
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservations";
+
+import React from 'react';
+
+import ReservationClient from "./ReservationClient";
+
+const ReservationPage = async () => {
+    const currentUser = await getCurrentUser();
+
+    if(!currentUser) {
+        return (
+            <ClientOnly>
+                <EmptyState
+                    title="unauthorized"
+                    subTitle="Please login"
+                />
+            </ClientOnly>
+        )
+    }
+
+    const reservations = await getReservations({
+        authorId: currentUser.id
+    });
+
+    if(reservations.length === 0) {
+        return (
+            <ClientOnly>
+                <EmptyState 
+                    title="No reservation found"
+                    subTitle="Looks like you have no reservation on your property."
+                />
+            </ClientOnly>
+        )
+    }
+
+    return (
+        <ClientOnly>
+            <ReservationClient 
+                reservations={reservations}
+                currentUser={currentUser}
+            />
+        </ClientOnly>
+    );
+};
+
+export default ReservationPage;
